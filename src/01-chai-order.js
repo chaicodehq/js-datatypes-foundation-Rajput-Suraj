5630915cc96622c5d420293424a9199c518fff9b
Rajput-Suraj <surajarajput@gmail.com>
2026-02-09 10:49:22 +0530

    Complete 01-chai-order

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -46,21 +46,41 @@
  *   hasSpecialIngredient("Elaichi Chai", "elaichi")  // => true
  */
 export function getChaiOrderLength(order) {
-  // Your code here
+  if (typeof order !== "string") return -1;
+
+  if (order.trim().length === 0) {
+    return 0;
+  } else {
+    return order.trim().length;
+  }
 }
 
 export function shoutChaiOrder(order) {
-  // Your code here
+  if (typeof order !== "string" || order.trim().length === 0) return "";
+
+  return order.trim().toUpperCase();
 }
 
 export function whisperChaiOrder(order) {
-  // Your code here
+  if (typeof order !== "string" || order.trim().length === 0) return "";
+
+  return order.trim().toLowerCase();
 }
 
 export function hasSpecialIngredient(order, ingredient) {
-  // Your code here
+  if (typeof order !== "string" || typeof ingredient !== "string") return false;
+
+  const lowerCasedOrder = order.toLowerCase();
+  const lowerCasedIngredient = ingredient.toLowerCase();
+
+  return lowerCasedOrder.includes(lowerCasedIngredient);
 }
 
 export function getFirstAndLastChar(order) {
-  // Your code here
+  if (typeof order !== "string" || order.trim().length === 0) return null;
+
+  return {
+    first: order.trim().charAt(0),
+    last: order.trim().at(-1),
+  };
 }
