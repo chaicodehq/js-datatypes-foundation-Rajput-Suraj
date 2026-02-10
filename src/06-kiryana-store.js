78e4e454cba804e7e0e1ac65790d219648591119
Rajput-Suraj <surajarajput@gmail.com>
2026-02-10 18:06:39 +0530

    Complete 06-kiryana-store

--- src/06-kiryana-store.js
+++ src/06-kiryana-store.js
@@ -51,21 +51,41 @@
  *   formatBill([{name:"Atta",price:40,qty:2}]) // => "Atta x 2 = Rs.80"
  */
 export function getItemNames(items) {
-  // Your code here
+  if (!Array.isArray(items)) return [];
+
+  return items?.map((item) => item?.name);
 }
 
 export function getAffordableItems(items, maxPrice) {
-  // Your code here
+  if (!Array.isArray(items) || typeof maxPrice !== "number") return [];
+
+  return items?.filter((item) => item?.price <= maxPrice);
 }
 
 export function calculateTotal(items) {
-  // Your code here
+  if (!Array.isArray(items) || items.length === 0) return 0;
+
+  return items.reduce((acc, curr) => acc + curr?.price * curr?.qty, 0);
 }
 
 export function sortByPrice(items, ascending) {
-  // Your code here
+  if (!Array.isArray(items)) return [];
+
+  return [...items].sort((a, b) => {
+    if (ascending) {
+      return a.price - b.price;
+    } else {
+      return b.price - a.price;
+    }
+  });
 }
 
 export function formatBill(items) {
-  // Your code here
+  if (!Array.isArray(items) || items.length === 0) return "";
+
+  return items
+    ?.map(({ name, qty, price }) => {
+      return `${name} x ${qty} = Rs.${qty * price}`;
+    })
+    .join("\n");
 }
