1daf973d50dd07e691d4f61d74b01fd27468650b
Rajput-Suraj <surajarajput@gmail.com>
2026-02-10 18:49:02 +0530

    Complete 08-paan-shop

--- src/08-paan-shop.js
+++ src/08-paan-shop.js
@@ -46,17 +46,35 @@
  *   updatePrices({meetha:30, saada:20}, 10)              // => {meetha:40, saada:30}
  */
 export function createPaanOrder(basePaan, customizations) {
-  // Your code here
+  if (typeof basePaan !== "object" || basePaan === null) return {};
+
+  if (typeof customizations !== "object") return { ...basePaan };
+
+  return Object.assign({}, basePaan, customizations);
 }
 
 export function freezeMenu(menu) {
-  // Your code here
+  if (typeof menu !== "object" || menu === null) return {};
+
+  return Object.freeze(menu);
 }
 
 export function updatePrices(menu, increase) {
-  // Your code here
+  if (typeof menu !== "object" || menu === null || typeof increase !== "number")
+    return {};
+
+  const updatedPrices = [];
+  for (const [key, value] of Object.entries(menu)) {
+    updatedPrices.push([key, value + increase]);
+  }
+
+  return Object.fromEntries(updatedPrices);
 }
 
 export function mergeDailySpecials(regularMenu, specialsMenu) {
-  // Your code here
+  const updatedRegularMenu = typeof regularMenu !== "object" ? {} : regularMenu;
+  const updatedSpecialMenu =
+    typeof specialsMenu !== "object" ? {} : specialsMenu;
+
+  return { ...updatedRegularMenu, ...updatedSpecialMenu };
 }
